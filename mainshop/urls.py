c786ec5c2b44317adead076bd4e3c6a88497fac6
Weverson Godinho <weverson.godinho@icloud.com>
2024-01-10 14:25:53 +0000

    feat(details_products): implement product detail page Created details_products.html to display product details, including price, color, and the 'add to bag' option. feat(details_products): add comment section for user interaction Added a comment section in details_products to enhance user engagement. style(details_products): apply Bootstrap and CSS styling Implemented styling using Bootstrap and CSS to enhance the visual presentation. feat(main_page): enable redirection to details_products.html Linked product images and 'Views' button in the main page to redirect to details_products.html upon clicking.

--- mainshop/urls.py
+++ mainshop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from mainshop.views import main_page
+from mainshop.views import main_page, details_product
 
 urlpatterns = [
-    path('', main_page)
+    path('', main_page, name='main_page'),
+    path('details', details_product, name='details-products'),
 ]
--- mainshop/views.py
+++ mainshop/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def main_page(request):
     return render(request, 'products/main.html')
+
+def details_product(request):
+    return render(request, 'products/details_product.html')
