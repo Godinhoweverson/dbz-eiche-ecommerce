cb541d096f59e3af96db210e5cfdbd3f9515d808
Weverson Godinho <weverson.godinho@icloud.com>
2024-02-09 14:52:18 +0000

    Remove duplicate Django tag

--- EicheBazaar/settings.py
+++ EicheBazaar/settings.py
@@ -214,8 +214,6 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'dbz-eiche.example.com'
 
 
-
-
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'dbz-eiche.example.com'
